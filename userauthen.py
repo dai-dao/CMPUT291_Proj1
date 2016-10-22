fa985ad3e5499b5d66dc2e2e4f01d239eeed4bbc
ReverseOni <fierce_deity888@hotmail.com>
2016-10-21 19:37:45 -0600

    Updated user authentication
    Allows for the capture of ID tag from table as the prefix for the
    username won't work due to the hashing of the username and password for
    comparision. Therefore ID_tags are added to the user table to
    circcumvent these restrictions.

--- userauthen.py
+++ userauthen.py
@@ -14,23 +14,47 @@ print "Welcome to the Hospital database.\n".upper()
 print "Please enter your username and password below to start\n"
 
 
+
+
+#loops if user enters username or password that is not in the database
 while True:
+    
     username_input = raw_input("username: ")
     password_input = raw_input("password: ")
 
 #hashing of the entered user inputs so that they may be compared against the database
     username = hashlib.sha224(username_input).hexdigest()
     password = hashlib.sha224(password_input).hexdigest()
+    
 
-#checks to see if the user inputs are within the table (and sets a flag for user type?)
+
+#CURRENT USERNAMES & PASSWORDS:
+# Doctor: username
+#         password
+#
+# Nurse:  password
+#         username
+#
+# admin:  admin
+#         admin
+    
+    #checks to see if the user inputs are within the table
     if re.match("^[A-Za-z0-9_]*$", username) and re.match("^[A-Za-z0-9_]*$", password):
-        c.execute("SELECT * FROM users WHERE username=? AND password=?;", (username, password))
-        id_exists = c.fetchone()
-        if id_exists:
-            print 'Hello "DNA" user'
-            break
+        c.execute("SELECT id FROM users WHERE username=? AND password=?;", (username, password))
+        
+        user_exists = c.fetchone()
+        if user_exists:
+            if user_exists[0] == 'A':
+                print '\nHello Administrator'
+                break
+            if user_exists[0] == 'D':
+                print '\nHello Doctor'
+                break            
+            if user_exists[0] == 'N':
+                print '\nHello Nurse'
+                break            
         else:
-            print "Your username or password is not correct, try again."
+            print "\nYour username or password is not correct, try again.\n"
     
   
 
